27e0b162cc5134ac0288d9b9de3a196d408581ec
Raghav-0007 <rmyadav514@gmail.com>
2025-02-25 17:02:17 +0530

    [Radhamohan yadav] . add created dto and model

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.employeepayrollapp.controllers;
 
 
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +23,11 @@ public class EmployeePayrollController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<String> addEmployeePayrollData(){
-        return new ResponseEntity<>("create employee data successful", HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO){
+        EmployeePayrollData empData=null;
+        empData=new EmployeePayrollData(1, empPayrollDTO.name, empPayrollDTO.salary);
+        ResponseDTO resDTO=new ResponseDTO("Created employee payroll data successfully", empData);
+        return new ResponseEntity<>(resDTO, HttpStatus.OK);
     }
 
     @PutMapping("/update")
---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+public class EmployeePayrollData {
+    private int employeeId;
+    private String name;
+    private long salary;
+
+    public EmployeePayrollData(){}
+
+    public EmployeePayrollData(int employeeId, String name, long salary){
+        this.employeeId=employeeId;
+        this.name=name;
+        this.salary=salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
