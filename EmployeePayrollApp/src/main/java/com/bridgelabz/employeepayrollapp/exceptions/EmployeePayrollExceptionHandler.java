01ff75e2c8ce6a5b6dfb279485491a416d3fd57e
Raghav-0007 <rmyadav514@gmail.com>
2025-03-01 13:12:54 +0530

    [Radhamohan yadav] . add handle incorrect data format

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employeepayrollapp.exceptions;
 
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -23,13 +26,19 @@ public class EmployeePayrollExceptionHandler {
                 .map(objectError -> objectError.getDefaultMessage())
                 .collect(Collectors.toList());
         ResponseDTO responseDTO=
-                new ResponseDTO("Exception while prossesing REST Request", errMessage);
+                new ResponseDTO("Exception while processing REST Request", errMessage);
         return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmployeePayrollException.class)
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception){
-        ResponseDTO resDTO=new ResponseDTO("Exception while prossesing REST Request", exception.getMessage());
+        ResponseDTO resDTO=new ResponseDTO("Exception while processing REST Request", exception.getMessage());
+        return new ResponseEntity<>(resDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("Invalid date format", exception);
+        ResponseDTO resDTO= new ResponseDTO("Exception while processing REST Request", "date should have in the format of dd MM yyyy");
         return new ResponseEntity<>(resDTO, HttpStatus.BAD_REQUEST);
     }
 }
